d8f935e48c2df288e7ae3dfef9dc2343bfd4c20f
Reski Aditya Desyawan <rickyadityad@gmail.com>
2015-10-02 09:48:46 +0700

    Tugas pertemuan ke 2
    18 agustus 2015 -

---
+++ WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form123
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.gantiButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'gantiButton
+        '
+        Me.gantiButton.Location = New System.Drawing.Point(193, 180)
+        Me.gantiButton.Name = "gantiButton"
+        Me.gantiButton.Size = New System.Drawing.Size(75, 23)
+        Me.gantiButton.TabIndex = 0
+        Me.gantiButton.Text = "Ganti"
+        Me.gantiButton.UseVisualStyleBackColor = True
+        '
+        'Form123
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Blue
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.gantiButton)
+        Me.Name = "Form123"
+        Me.Text = "Form Reski"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents gantiButton As System.Windows.Forms.Button
+
+End Class
---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form123
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles gantiButton.Click
+        If Me.BackColor = Color.Blue Then
+            Me.BackColor = Color.Red
+        ElseIf Me.BackColor = Color.Red Then
+            Me.BackColor = Color.Blue
+        End If
+    End Sub
+End Class
